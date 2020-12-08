17e208c944c027a9666e592a707ce56ac7b978d7
ITNood <1066770243@qq.com>
2020-12-08 13:03:44 +0800

    gov

---
+++ src/components/nav.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div id="header">
+      <div class="container">
+          <!--logo-->
+        <div class="logo">
+          <img src="../static/images/logo.png" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  name: "Nav",
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
+};
+</script>
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <Nav/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Nav from "../components/nav";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  components: {Nav},
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
+};
 </script>
+<style scoped>
+</style>
